ffcd9aa71bcacd03606553888c2b3fd7813fce2a
Siddharth Roy <siddharthroy36912@gmail.com>
2021-08-26 12:05:22 +0530

    Fixed netlify build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withTM = require('next-transpile-modules')(['react-syntax-highlighter', 'react-markdown', 'remark-gfm']);
 
 module.exports = withTM({
-  target: 'serverless',
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com'],
