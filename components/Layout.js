d8be28af222bc2f49692fd6a16e898076cdfbae7
Siddharth Roy <siddharthroy36912@gmail.com>
2022-01-16 13:43:34 +0530

    Added footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar'
+import UpIcon from '../components/icons/Up'
 import styles from '../styles/Layout.module.css'
 
 export default function Layout({ children }) {
@@ -8,6 +9,17 @@ export default function Layout({ children }) {
 				<Navbar />
 				{children}
 			</div>
+			<footer className={styles.footer}>
+				<div className={styles.container}>
+					<p className={styles.copyright}>
+						© Siddharth Roy, 2022
+					</p>
+					<div className={styles.footer__right}>
+						<UpIcon />
+						<p>Back To Top</p>
+					</div>
+				</div>
+			</footer>
 		</>
 	)
 }
